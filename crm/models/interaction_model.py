e62d29b353befa3383920b4e811a8aeeed6aea57
Oconcejero <oconcejero@gmail.com>
2026-02-26 09:47:26 +0100

    Primer commit

---
+++ crm/models/interaction_model.py
@@ -0,0 +1,15 @@
+from django.db import models
+from .client_model import Client
+
+
+class Interaction(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='interactions')
+    date = models.DateTimeField(auto_now_add=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        try:
+            return f"Interaction with {self.client.first_name} on {self.date.strftime('%Y-%m-%d %H:%M')}"
+        except:
+            return "Interaction"
+
